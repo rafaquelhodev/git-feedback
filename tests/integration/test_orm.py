04225e341893d63c914f07a7ecd8cf045dd56cfe
Rafael <rafael.quelho@gmail.com>
2021-11-28 15:47:28 -0300

    tests: update unit tests and and integration tests

---
+++ tests/integration/test_orm.py
@@ -0,0 +1,63 @@
+from app.domain import model
+
+
+def test_can_insert_git_repo(session):
+    session.add(model.GitRepo(name="api-foo", url="github.com/api-foo", user_id=1))
+    session.commit()
+
+    [git_repo] = session.execute(
+        "SELECT * FROM git_repos WHERE id=:id",
+        dict(id=1),
+    )
+
+    assert git_repo.name == "api-foo"
+    assert git_repo.url == "github.com/api-foo"
+    assert git_repo.user_id == 1
+
+
+def test_query_git_feedbacks(session):
+    session.execute(
+        "INSERT INTO git_repos (name, url, user_id) VALUES "
+        '("api-foo", "github.com/api-foo", 1)'
+    )
+    session.execute(
+        "INSERT INTO feedbacks (message, giver_id, git_repo_id, private) VALUES "
+        '("cool repo", 2, 1, False)'
+    )
+
+    git_repo = session.query(model.GitRepo).all()[0]
+
+    feebacks = git_repo.get_feedbacks()
+    assert len(feebacks) == 1
+    assert feebacks[0].message == "cool repo"
+    assert feebacks[0].private == False
+
+
+def test_insert_git_feedbacks(session):
+    session.execute(
+        "INSERT INTO git_repos (name, url, user_id) VALUES "
+        '("api-foo", "github.com/api-foo", 1)'
+    )
+
+    feedback = model.Feedback(message="cool repo :)", giver_id=2)
+    git_repo = session.query(model.GitRepo).all()[0]
+    git_repo.receive_feedback(feedback)
+    session.commit()
+
+    [res_feedback] = session.execute("SELECT * FROM feedbacks")
+
+    assert res_feedback.message == "cool repo :)"
+    assert res_feedback.private == False
+
+
+def test_can_query_git_repo(session):
+    session.execute(
+        "INSERT INTO git_repos (name, url, user_id) VALUES "
+        '("api-foo", "github.com/api-foo", 10),'
+        '("api-bar", "gitlab.com/api-bar", 20),'
+        '("api-foo-bar", "github.com/api-foo-bar", 30)'
+    )
+
+    git_repos = session.query(model.GitRepo).all()
+
+    assert len(git_repos) == 3
--- tests/unit/test_domain.py
+++ tests/unit/test_domain.py
@@ -5,7 +5,7 @@ from app.domain.model import Feedback, User, GitRepo
 
 class TestFeedback:
     def test_user_should_import_git_repo(self):
-        user = User(name="John", email="john@test.com")
+        user = User(id=1, name="John", email="john@test.com")
         git_repo = GitRepo(
             name="repo-name", url="https://www.gitlab.com/test", user_id=user.id
         )
@@ -13,18 +13,18 @@ class TestFeedback:
         assert len(git_repo.get_feedbacks()) == 0
 
     def test_user_gives_feedback(self):
-        receiver = User(name="John", email="john@test.com")
+        receiver = User(id=1, name="John", email="john@test.com")
         git_repo = GitRepo(
             name="repo-name", url="https://www.gitlab.com/test", user_id=receiver.id
         )
 
-        giver = User(name="Marry", email="marry@test.com")
+        giver = User(id=2, name="Marry", email="marry@test.com")
         giver.give_feedback(git_repo, "your repo is great!", True)
 
         assert len(git_repo.get_feedbacks()) == 1
 
     def test_same_user_cannot_give_feedback_for_own_repo(self):
-        receiver = User(name="John", email="john@test.com")
+        receiver = User(id=1, name="John", email="john@test.com")
         git_repo = GitRepo(
             name="repo-name", url="https://www.gitlab.com/test", user_id=receiver.id
         )

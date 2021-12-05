1fdf6c77dd75c8e8550998b6d2877c54f85424dd
Rafael <rafael.quelho@gmail.com>
2021-12-05 15:26:49 -0300

    test: add new tests for user registration

--- tests/unit/test_domain_usecases.py
+++ tests/unit/test_domain_usecases.py
@@ -1,3 +1,6 @@
+import pytest
+from app.domain.exceptions import InvalidUserRegistration
+from app.domain.model import User
 from app.domain_usecases.register_user import RegisterUser
 from app.adapters.password_passlib import PasswordPassLib
 from app.ports.user_repository_memory import UserRepositoryMemory
@@ -23,3 +26,43 @@ class TestRegisterUser:
         assert user.name == "John"
         assert user.email == "john@test.com"
         assert user.password != "123456"
+
+    def test_error_when_username_is_used(self):
+        user_repo = UserRepositoryMemory()
+
+        user_john = User(name="John", email="john@test.com", password="123")
+        user_repo.add(user_john)
+
+        password_lib = PasswordPassLib()
+
+        register_user = RegisterUser(
+            user_repository=user_repo,
+            password_lib=password_lib,
+            username="John",
+            email="john_2@test.com",
+            password="123",
+        )
+
+        with pytest.raises(InvalidUserRegistration) as error:
+            register_user.execute()
+            assert "User name or email already in use" in str(error.value)
+
+    def test_error_when_email_is_used(self):
+        user_repo = UserRepositoryMemory()
+
+        user_john = User(name="Mary", email="mary@test.com", password="123")
+        user_repo.add(user_john)
+
+        password_lib = PasswordPassLib()
+
+        register_user = RegisterUser(
+            user_repository=user_repo,
+            password_lib=password_lib,
+            username="mary-jane",
+            email="mary@test.com",
+            password="123",
+        )
+
+        with pytest.raises(InvalidUserRegistration) as error:
+            register_user.execute()
+            assert "User name or email already in use" in str(error.value)
